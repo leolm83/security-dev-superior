beee16fe7c4d54008f56ef58c092fbf337e4ec61
leolm83 <leonardo.mateus01@hotmail.com>
2024-07-05 10:26:38 -0300

    removendo teste bcrypt

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -10,16 +10,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class DemoApplication {
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@EventListener(ApplicationReadyEvent.class)
-	public void run(){
-		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
-		boolean result = passwordEncoder.matches("12356","$2a$10$k6bVJ5iQ7xKzoZOlxofbj.bJEGMoyiZRBtmZ5nX3iAW3Cf2b8gas6");
-		System.out.println("RESULTADO = " + result);
-	}
+
 }
